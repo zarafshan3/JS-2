03e40820bcdcfc4ceb8236b54ef66ff68c892ff2
unknown <ejazzarafshan@gmail.com>
2024-07-19 01:18:36 +0400

    hehe

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+ function chBgColorW(){
+    document.body.style.background = "red"
+    document.getElementById('button1').style.color ="orange"
+ }
+
+ function chColor(color){
+    document.body.style.background = color
+ }
+
+ function openChatbox(){
+    document.getElementById('chatbox').style.right = "0"
+ }
